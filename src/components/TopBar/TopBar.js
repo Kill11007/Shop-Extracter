ec69a832c1c17f77421247e4630a5eab3877201f
mayank <mj112295@gmail.com>
2022-01-28 21:04:56 +0530

    changed css

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,7 +4,7 @@ import './TopBar.css';
 function TopBar() {
     return (
         <div className='Profile-Baba-Logo'>
-            <img className='profile-baba-image' src='https://profilebaba.com/public/image/logo.png' alt='profile-baba-image' />
+            <img className='profile-baba-image' src='https://profilebaba.com/public/image/logo.png' alt='profile-baba-logo' />
             <div>
                 <span className='title-profile'>Profile</span>
                 <span className='title-baba'>Baba</span>
